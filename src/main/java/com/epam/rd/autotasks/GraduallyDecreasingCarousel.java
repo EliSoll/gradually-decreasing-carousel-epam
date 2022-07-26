a1478cb516691ea5af727a9174792baef878444d
Elvira Solnyshkina <elvira.solnyshkina@gmail.com>
2022-07-26 20:27:35 +0300

    test 1

--- src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
+++ src/main/java/com/epam/rd/autotasks/GraduallyDecreasingCarousel.java
@@ -1,7 +1,41 @@
 package com.epam.rd.autotasks;
 
+import static com.epam.rd.autotasks.DecrementingCarousel.array;
+
 public class GraduallyDecreasingCarousel extends DecrementingCarousel{
     public GraduallyDecreasingCarousel(final int capacity) {
+
         super(capacity);
     }
+
+    public DecrementingRun run() {
+        if (isRunning) return null;
+        isRunning = true;
+        return new DecrementingRun(this);
+    }
 }
+
+class DecrementingRun extends CarouselRun {
+    int position = 0;
+
+    public DecrementingRun(GraduallyDecreasingCarousel decrementingRun) {
+        super(decrementingRun);
+    }
+    @Override
+    public int next() {
+        int temp;
+        if(isFinished()) {
+            return -1;
+        }
+        else {
+            while (array[position %= array.length] <= 0) {
+                position++;
+            }
+        }
+        temp = array[position];
+        int n = 1;
+        array[position++]-=n;
+        return temp;
+    }
+}
+
